32254dadc728171751d15891745b1ceda2e0a01f
Alois Wollersberger <alois.wollersberger@9yards.at>
2017-09-22 09:40:56 +0200

    make model public for proguard

--- analytics/src/main/java/at/nineyards/analytics/model/AnalyticsConstant.kt
+++ analytics/src/main/java/at/nineyards/analytics/model/AnalyticsConstant.kt
@@ -3,7 +3,7 @@ package at.nineyards.analytics.model
 /**
  * Created by Lois-9Y on 19/09/2017.
  */
-internal data class AnalyticsConstant(
+data class AnalyticsConstant(
         val name : String,
         val value : String
 )
--- analytics/src/main/java/at/nineyards/analytics/model/AnalyticsDefinition.kt
+++ analytics/src/main/java/at/nineyards/analytics/model/AnalyticsDefinition.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.Json
 /**
  * Created by Lois-9Y on 19/09/2017.
  */
-internal data class AnalyticsDefinition(
+data class AnalyticsDefinition(
         @Json(name = "event_list") val eventList : List<AnalyticsEvent>,
         @Json(name = "parameter_list") val parameterList : List<AnalyticsParameter>,
         @Json(name = "constant_list") val constantList : List<AnalyticsConstant>
